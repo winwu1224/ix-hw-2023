225dd83172b7f1ca0cf6dc8a36b62740ccc8ad3b
jricciardulli <joseph_ricciardulli@brown.edu>
2023-05-31 20:24:07 +0200

    added joeys sequence

--- ix-hw-2023/day_3/index.js
+++ ix-hw-2023/day_3/index.js
@@ -18,3 +18,24 @@ function fibonacci() {
 }
 
 fibonacci();
+
+//Joey's sequence
+function fib() {
+  let counter = 0;
+  let prev = 1;
+  let next = 1;
+
+  console.log(0);
+  console.log(prev);
+  console.log(next);
+
+  while (counter < 7) {
+    let num = prev + next;
+    prev = next;
+    next = num;
+    console.log(num);
+    counter++;
+  }
+}
+
+fib();
